50efe8170d9e8e4e57cba7ba99f02930fb9e683c
parent41 <oparent456@gmail.com>
2023-11-03 16:45:47 -0400

    write ids

--- scripts/bison_matrices.R
+++ scripts/bison_matrices.R
@@ -11,13 +11,17 @@ print(args)
 # args=c()
 # args[1] = "./micro_file_lists/sub6_ses2_Label.txt"
 # args[2] = "../UKB/temporary_template/Mask_2mm_dil2.mnc"
-# args[3] = "./bison_matrices/bison_wholeBrain_ses2.tsv"
+# args[3] = "./bison_matrices/sub6_ses2_Label_whole_brain.tsv"
 
 # Load mask
 mask=mincGetVolume(args[2])
 
 fl=read.table(args[1])
 fl=c(fl$V1)
+
+ids = sub('.*sub-(\\d+)_ses.*', '\\1', fl)
+ids_filename = sub('.*/([^/]+)\\..*', '\\1', args[3])
+fwrite(as.data.frame(ids), paste0("./bison_matrices/ids_",ids_filename,".txt"), row.names=FALSE, col.names=FALSE, sep='\t', quote=FALSE ) # List of subjects included
 
 mat = matrix(nrow=length(fl), ncol=sum(mask[] == 1))
 
