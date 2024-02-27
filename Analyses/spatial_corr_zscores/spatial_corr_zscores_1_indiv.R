35f504f3eddc805e23b09794efd013ffc05bc127
parent41 <oparent456@gmail.com>
2024-02-27 09:47:38 -0500

    first commit

---
+++ Analyses/spatial_corr_zscores/spatial_corr_zscores_1_indiv.R
@@ -0,0 +1,121 @@
+
+library(data.table)
+library(RMINC)
+# install.packages("Hmisc")
+library(Hmisc)
+library(foreach)
+library(doParallel)
+
+args = commandArgs(trailingOnly=TRUE)
+
+dir.create("./visualization/indiv", showWarnings=FALSE)
+dir.create("./results/indiv", showWarnings=FALSE)
+
+# args = c()
+# args[1] = 32
+
+num_chunk = as.numeric(args[1])
+
+names = c("MD", "ISOVF", "FA", "ICVF", "OD", "T2star", "QSM", "jacobians_abs", "jacobians_rel")
+names_long = c("dti_MD_UKB", "NODDI_ISOVF_UKB", "dti_FA_UKB", "NODDI_ICVF_UKB", "NODDI_OD_UKB", "T2star_UKB", "QSM_UKB", "jacobians_abs_2mm", "jacobians_rel_2mm")
+tissue_all=c('Ventricules', 'CSF', 'Cerebellum_GM', 'Cerebellum_WM', 'Brainstem', 'Subcortical_GM', 'Cortical_GM', 'Cerebral_NAWM', 'WMH')
+
+mask = mincGetVolume("../../../UKB/temporary_template/Mask_2mm_dil2.mnc")
+
+# Load data
+
+inclusions = as.data.frame(fread(paste0("../subj_zscores_common_all/tmp/ids_micro_c",num_chunk,"_FA_anlm.txt")))
+colnames(inclusions) = "ID"
+ids = inclusions$ID
+
+# Spatial correlations
+
+flattenCorrMatrix <- function(cormat, pmat) {
+  ut <- upper.tri(cormat)
+  data.frame(
+    Marker_1 = rownames(cormat)[row(cormat)[ut]],
+    Marker_2 = rownames(cormat)[col(cormat)[ut]],
+    cor  =(cormat)[ut],
+    p = pmat[ut]
+    )
+}
+
+get_spatial_corrs = function(label_df, micro_df) {
+    
+    # First batch of results with label = 3
+    micro_tmp = micro_df[which(label_df[mask[]>0.5] == 3),]
+    corrs = rcorr(as.matrix(micro_tmp))
+    corrs = flattenCorrMatrix(corrs$r, corrs$P)
+    corrs$Label = tissue_all[3]
+    result = corrs
+
+    # For the rest of the labels...
+    for (l in 4:9) {
+        micro_tmp = micro_df[which(label_df[mask[]>0.5] == l),]
+        corrs = rcorr(as.matrix(micro_tmp))
+        corrs = flattenCorrMatrix(corrs$r, corrs$P)
+        corrs$Label = tissue_all[l]
+        result = rbind(result, corrs)
+    }
+
+    # Across the whole brain
+    micro_tmp = micro_df[which(label_df[] %in% seq(3,9)),]
+    corrs = rcorr(as.matrix(micro_tmp))
+    corrs = flattenCorrMatrix(corrs$r, corrs$P)
+    corrs$Label = "Whole_brain"
+    result = rbind(result, corrs)
+
+    # Across the whole cerebral white matter
+    micro_tmp = micro_df[which(label_df[] %in% seq(8,9)),]
+    corrs = rcorr(as.matrix(micro_tmp))
+    corrs = flattenCorrMatrix(corrs$r, corrs$P)
+    corrs$Label = "Cerebral_WM"
+    result = rbind(result, corrs)
+
+    return(result)
+}
+
+# Run
+cores = detectCores()
+parallelCluster <- makeCluster(cores-5, type = "SOCK", methods = FALSE)
+setDefaultCluster(parallelCluster)
+registerDoParallel(parallelCluster)
+
+foreach(i=1:length(ids), .packages = c('RMINC', 'data.table', 'Hmisc')) %dopar% {
+# for (i in 1:length(ids)) {
+    cat(paste0("\nID = ", ids[i], "\n"))
+
+    label_id = round(mincGetVolume(paste0("../../../WMH_micro_spatial/maps_UKB_space/sub-",ids[i],"_ses-2_Label_UKB.mnc")),0)
+
+    # For zscores
+    zscores_id = as.data.frame(matrix(nrow=length(label_id[mask>0.5]), ncol=length(names)))
+    colnames(zscores_id) = names
+    for (n in 1:length(names)) {
+        zscores_id_tmp = mincGetVolume(paste0("../subj_zscores_common_all/results/mnc/",ids[i],"_zscore_anlm_",names[n],".mnc"))
+        zscores_id[,n] = zscores_id_tmp[mask[]>0.5]
+    }
+
+    corrs_id = get_spatial_corrs(label_id, zscores_id)
+    corrs_id = cbind(ids[i], corrs_id)
+    colnames(corrs_id)[1] = "ID"
+
+    fwrite(as.data.frame(corrs_id), paste0("./results/indiv/",ids[i],"_spatial_corrs_zscores.tsv"), col.names=TRUE, row.names=FALSE, quote=FALSE, sep="\t")
+
+    # For raw micro
+    micro_id = as.data.frame(matrix(nrow=length(label_id[mask>0.5]), ncol=length(names)))
+    colnames(micro_id) = names
+    for (n in 1:length(names)) {
+        micro_id_tmp = mincGetVolume(paste0("../../maps_UKB_space_anlm_all/sub-",ids[i],"_ses-2_",names_long[n],"_anlm.mnc"))
+        micro_id[,n] = micro_id_tmp[mask[]>0.5]
+    }
+
+    corrs_id = get_spatial_corrs(label_id, micro_id)
+    corrs_id = cbind(ids[i], corrs_id)
+    colnames(corrs_id)[1] = "ID"
+
+    fwrite(as.data.frame(corrs_id), paste0("./results/indiv/",ids[i],"_spatial_corrs_raw.tsv"), col.names=TRUE, row.names=FALSE, quote=FALSE, sep="\t")
+
+}
+
+
+
---
+++ Analyses/spatial_corr_zscores/spatial_corr_zscores_2_indiv_viz.R
@@ -0,0 +1,85 @@
+
+library(data.table)
+library(tidyverse)
+library(dplyr)
+library(ggplot2)
+library(patchwork)
+library(viridis)
+library(Hmisc)
+library(corrplot)
+
+names = c("MD", "ISOVF", "FA", "ICVF", "OD", "T2star", "QSM", "jacobians_rel", "jacobians_abs")
+regions = c('Whole_brain', 'Cerebellum_GM', 'Cerebellum_WM', 'Brainstem', 'Subcortical_GM', 'Cortical_GM', 'Cerebral_NAWM', 'WMH', 'Cerebral_WM')
+
+# Load data
+
+corr_r = as.data.frame(fread("./results/indiv_concat_raw.tsv"))
+corr_z = as.data.frame(fread("./results/indiv_concat_zscores.tsv"))
+
+# corr_r[,c(1,2,3,6)] = as.data.frame(lapply(corr_r[,c(1,2,3,6)], as.factor))
+# corr_z[,c(1,2,3,6)] = as.data.frame(lapply(corr_z[,c(1,2,3,6)], as.factor))
+
+mypalette = colorRampPalette(rev(c("#67001F", "#B2182B", "#D6604D", "#F4A582",
+                                   "#FDDBC7", "#FFFFFF", "#D1E5F0", "#92C5DE",
+                                   "#4393C3", "#2166AC", "#053061")))
+
+
+# Correlation matrices by region
+
+corrmat_by_reg = function(df, type) {
+
+    # Correlation matrices for each region
+    for (l in 1:length(levels(factor(df$Label)))) {
+        print(levels(factor(df$Label))[l])
+
+        df_corr = df %>%
+            filter(Label == levels(factor(df$Label))[l]) %>%
+            bind_rows(
+                df %>%
+                    filter(Label == levels(factor(df$Label)[l])) %>%
+                    mutate(across(c(Marker_1, Marker_2), ~ ifelse(Marker_1 == ., Marker_2, Marker_1)))
+            ) %>%
+            mutate(marker_1_2 = factor(paste0(Marker_1,"_with_",Marker_2))) %>%
+            group_by(marker_1_2) %>%
+            dplyr::summarize(mean = mean(cor, na.rm=TRUE)) %>%
+            separate(marker_1_2, into = c("Marker_1", "Marker_2"), sep = "_with_") %>%
+            arrange(factor(Marker_1, levels = names), factor(Marker_2, levels = names)) %>%
+            pivot_wider(names_from = Marker_2, values_from = mean) %>%
+            column_to_rownames("Marker_1") %>%
+            select(names) #%>%
+            # glimpse()
+
+        png(height=20, width=20, units="in", res=300, pointsize=10, file=paste0("./visualization/corrmat_by_region/corrmat_",type,"_",levels(factor(df$Label))[l],".png"))
+        corrplot(as.matrix(df_corr), type="full", order="original", method="circle", addCoef.col = "black", addgrid.col = TRUE,
+                tl.srt = 45, tl.col = "black", col=mypalette(200), diag=FALSE, tl.cex=3, cl.length = 5, cl.cex = 3, number.cex=3, cex.main=5,
+                title=levels(factor(df$Label))[l], mar=c(0,0,4,0), )
+        dev.off()
+    }
+
+    # Append correlation matrices
+    command = paste0("convert -gravity center ")
+    for (l in 1:length(levels(factor(df$Label, levels=regions)))) {
+        command = paste0(command, "./visualization/corrmat_by_region/corrmat_",type,"_",levels(factor(df$Label, levels=regions))[l],".png ")
+    }
+    command = paste0(command, "-append ./visualization/corrmat_by_region/corrmat_",type,"_allregions.png")
+    system(command)
+}
+
+dir.create("./visualization/corrmat_by_region", showWarnings=FALSE)
+
+# Run for raw and zscores
+corrmat_by_reg(corr_r, "raw")
+corrmat_by_reg(corr_z, "zscores")
+
+# Append
+command = paste0("convert -gravity center ./visualization/corrmat_by_region/corrmat_raw_allregions.png ./visualization/corrmat_by_region/corrmat_zscores_allregions.png +append ./visualization/corrmat_by_region/corrmat_all_allregions.png")
+system(command)
+
+
+
+
+
+
+
+
+
